fafccfa1b8fc909131e4dc5095928e9bb4e7f663
Dharam Soni <dharamsoni1010@gmail.com>
2024-12-25 03:12:17 +0530

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import About from "./components/About"
 import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <main>
+      <Navbar />
       <Hero />
       <About />
     </main>
