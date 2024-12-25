3b328053df9bbbd145e4da771235441d5e577314
Dharam Soni <dharamsoni1010@gmail.com>
2024-12-25 13:07:57 +0530

    Fixed navbar padding and added metadata

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,10 +26,10 @@ const socialLinks: FooterLinks[] = [
 
 const Footer = () => {
   return (
-    <footer className="w-screen bg-[#5542ff] py-4 text-black">
+    <footer className="w-screen bg-[#5542ff] py-4 text-black lg:pr-4">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          &copy; Undead Reaper 2024. All rights reserved.
+          &copy; 2024. All rights reserved.
         </p>
 
         <div className="flex justify-center gap-4  md:justify-start">
